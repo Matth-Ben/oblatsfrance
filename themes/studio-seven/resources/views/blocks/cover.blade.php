164a5ba6dbb34731ca56faa5e7ee2427e4a94d63
Matth-Ben <matthias.benoit@outlook.fr>
2022-11-20 16:07:58 +0100

    🐛 [COVER] Block to page element

--- themes/studio-seven/resources/views/blocks/cover.blade.php
+++ themes/studio-seven/resources/views/blocks/cover.blade.php
@@ -12,22 +12,24 @@
 @endphp
 
 <section class="b-cover">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="b-cover__body">
-        <div class="b-cover__item">
-          <div class="b-cover__item-background">
-            @include('elements/image', ['data' => $data['image']])
-          </div>
-          <div class="b-cover__item-content">
-            @if ($data['display'])
-              <div class="b-cover__item-breadcrumb">
-                @php do_shortcode('[wpseo_breadcrumb]') @endphp
-              </div>
-            @endif
-            <h1 class="b-cover__item-title e-title">
-              {!! $data['title'] !!}
-            </h1>
+  <div class="b-cover__content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="b-cover__body">
+          <div class="b-cover__item">
+            <div class="b-cover__item-background">
+              @include('elements/image', ['data' => $data['image']])
+            </div>
+            <div class="b-cover__item-content">
+              @if ($data['display'])
+                <div class="b-cover__item-breadcrumb">
+                  @php do_shortcode('[wpseo_breadcrumb]') @endphp
+                </div>
+              @endif
+              <h1 class="b-cover__item-title e-title">
+                {!! $data['title'] !!}
+              </h1>
+            </div>
           </div>
         </div>
       </div>
--- themes/studio-seven/resources/views/partials/content-page.blade.php
+++ themes/studio-seven/resources/views/partials/content-page.blade.php
@@ -1,1 +1,19 @@
-<div data-router-view="page">{!! the_content() !!}</div>
+@php
+  $field = get_fields(get_the_id());
+  $data = Block::cover($block['data'] = $field);
+@endphp
+
+<div class="page" data-router-view="page">
+  @if ($data)
+    <div class="page-head">
+      @include('blocks/cover', ['data' => $data])
+    </div>
+  @endif
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-24 col-lg-18">{!! the_content() !!}</div>
+      <div class="col-24 col-lg-6"></div>
+    </div>
+  </div>
+</div>
