6d7fbec35ca4159ddfd7f201b206ed371b305f6b
Matth-Ben <matthias.benoit@outlook.fr>
2022-11-20 16:07:15 +0100

    🐛 [COVER-SLIDER] Block to page element

--- themes/studio-seven/resources/views/blocks/cover-slider.blade.php
+++ themes/studio-seven/resources/views/blocks/cover-slider.blade.php
@@ -12,58 +12,60 @@
 @endphp
 
 <section class="b-cover-slider">
-  <div class="container-fluid u-h100">
-    <div class="row u-h100">
-      <div class="col-24">
-        <div class="b-cover-slider__imgs">
-          @foreach ($data['slides'] as $repeater)
-            <div class="b-cover-slider__imgs-item">
-              <div class="wrapper">
-                <div class="u-h100">
-                  @include('elements/image', ['data' => $repeater['image']])
-                </div>
-              </div>
-            </div>
-          @endforeach
-        </div>
-        <div class="row">
-          <div class="b-cover-slider__wrapper">
-            <div class="b-cover-slider__items">
-              @foreach ($data['slides'] as $repeater)
-                <div class="b-cover-slider__item @if($loop->index === 0){{ 'active' }}@endif">
-                  <div class="b-cover-slider__item-content">
-                  @if(!empty($repeater['title']))
-                    <div class="b-cover-slider__item-content-title">
-                      <@if ($loop->index === 0){{'h1'}}@else{{'h2'}}@endif class="e-title">{!! $repeater['title'] !!}</@if ($loop->index === 0){{'h1'}}@else{{'h2'}}@endif>
-                    </div>
-                  @endif
-
-                  @if(!empty($repeater['content']))
-                    <div class="b-cover-slider__item-content-content">
-                      {!! wpautop($repeater['content']) !!}
-                    </div>
-                  @endif
-
-                  @if(!empty($repeater['button']))
-                    <div class="b-cover-slider__item-content-button">
-                      <a href="{{ $repeater['button']['url'] }}" class="button" @if($repeater['button']['target']){{ 'target="_blank" rel="noopener"' }}@endif>
-                        {!! $repeater['button']['title'] !!}
-                      </a>
-                    </div>
-                  @endif
-
+  <div class="b-cover-slider__content">
+    <div class="container-fluid u-h100">
+      <div class="row u-h100">
+        <div class="col-24">
+          <div class="b-cover-slider__imgs">
+            @foreach ($data['slides'] as $repeater)
+              <div class="b-cover-slider__imgs-item">
+                <div class="wrapper">
+                  <div class="u-h100">
+                    @include('elements/image', ['data' => $repeater['image']])
                   </div>
                 </div>
-              @endforeach
+              </div>
+            @endforeach
+          </div>
+          <div class="row">
+            <div class="b-cover-slider__wrapper">
+              <div class="b-cover-slider__items">
+                @foreach ($data['slides'] as $repeater)
+                  <div class="b-cover-slider__item @if($loop->index === 0){{ 'active' }}@endif">
+                    <div class="b-cover-slider__item-content">
+                    @if(!empty($repeater['title']))
+                      <div class="b-cover-slider__item-content-title">
+                        <@if ($loop->index === 0){{'h1'}}@else{{'h2'}}@endif class="e-title">{!! $repeater['title'] !!}</@if ($loop->index === 0){{'h1'}}@else{{'h2'}}@endif>
+                      </div>
+                    @endif
+  
+                    @if(!empty($repeater['content']))
+                      <div class="b-cover-slider__item-content-content">
+                        {!! wpautop($repeater['content']) !!}
+                      </div>
+                    @endif
+  
+                    @if(!empty($repeater['button']))
+                      <div class="b-cover-slider__item-content-button">
+                        <a href="{{ $repeater['button']['url'] }}" class="button" @if($repeater['button']['target']){{ 'target="_blank" rel="noopener"' }}@endif>
+                          {!! $repeater['button']['title'] !!}
+                        </a>
+                      </div>
+                    @endif
+  
+                    </div>
+                  </div>
+                @endforeach
+              </div>
             </div>
           </div>
+          @if (count($data['slides']) > 1)
+            <div class="b-cover-slider__nav">
+              <div class="b-cover-slider__nav-left disabled">{{ display_svg('arrow') }}</div>
+              <div class="b-cover-slider__nav-right">{{ display_svg('arrow') }}</div>
+            </div>
+          @endif
         </div>
-        @if (count($data['slides']) > 1)
-          <div class="b-cover-slider__nav">
-            <div class="b-cover-slider__nav-left disabled">{{ display_svg('arrow') }}</div>
-            <div class="b-cover-slider__nav-right">{{ display_svg('arrow') }}</div>
-          </div>
-        @endif
       </div>
     </div>
   </div>
--- themes/studio-seven/resources/views/front-page.blade.php
+++ themes/studio-seven/resources/views/front-page.blade.php
@@ -1,9 +1,24 @@
 @extends('layouts.app')
 
+@php
+  $field = get_field('slides', get_the_id());
+  $data = Block::coverSlider($block['data'] = $field);
+@endphp
+
 @section('content')
   <div data-router-view="page">
     <div class="home">
-      {!! the_content() !!}
+      <div class="home-head">
+        @include('blocks/cover-slider', ['data' => $data])
+      </div>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-22 col-lg-15 offset-1">
+            {!! the_content() !!}
+          </div>
+          <div class="col-24 col-lg-7 offset-1"></div>
+        </div>
+      </div>
     </div>
   </div>
 @endsection
